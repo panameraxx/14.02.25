46797469d3abfdfe219ae5f7f473a4d69cae0d37
panameraxx <90213411+panameraxx@users.noreply.github.com>
2025-02-20 10:10:27 +0200

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle(5,76);
+            rectangle.ShowInfo();
+            
+            double adin = double.Parse(Console.ReadLine());
+            double dwa = double.Parse(Console.ReadLine());
+            Rectangle rectangle2 = new Rectangle(adin, dwa);
+            Console.WriteLine($"{rectangle2.Tostring()}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp2/Rectangle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Rectangle
+    {
+        private double length;
+        private double width;
+
+
+        public Rectangle() { }
+        public Rectangle(double length,double width)
+        {
+            this.Length = length;
+            this.Width = width;
+
+        }
+
+
+        public double Length
+        {
+            get => length; set
+            {
+                if (value < 0) throw new ArgumentException("error"); length = value;
+            }
+        }
+        public double Width { get => width; set
+            {
+                if (value < 0) throw new ArgumentException("error"); width = value;
+            }
+        }
+        public double Square()
+        {
+            return (length*width);
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Первое число: {length}\t второе число: {width}\t Результат{Square():f2}");
+        }
+
+        public string Tostring()
+        {
+            return $"Первое число: {length}\t второе число: {width}\t Результат{Square():f2}";
+        }
+    }
+}
