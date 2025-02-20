6d6fab9a364b7c1289bb28fb2623cde6a12b81cb
panameraxx <90213411+panameraxx@users.noreply.github.com>
2025-02-20 10:09:51 +0200

    Add files via upload

---
+++ ConsoleApp1/Calculate.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Calculate
+    {
+        private double number1;
+        private double number2;
+        public double Number1 { get => number1; 
+            set 
+            {
+                if (value < 0) throw new ArgumentException("error"); number1 = value; 
+            }
+        }
+
+        public double Number2 { get => number2; 
+            set
+            {
+                if (value < 0) throw new ArgumentException("error"); number1 = value;
+            }
+        }
+        
+
+        public Calculate() { }
+        public Calculate(double Number1)
+        {
+            this.Number1 = Number1;
+        }
+        public Calculate(double Number1, double Number2) : this(Number1)
+        {
+            this.Number2 = Number2;
+        }
+
+        public double Calculatekoren()
+        {
+            return Math.Sqrt(Number1 * Number2);
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Первое число: {Number1}\t второе число: {Number2}\t Результат{Calculatekoren():f2}");
+        }
+        public string Tostring()
+        {
+            return $"Первое число: {Number1}\t второе число: {Number2}\t Результат{Calculatekoren():f2}";
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate calc = new Calculate();
+            calc.Number1 = 10;
+            calc.Number2 = 2;
+            calc.ShowInfo();
+            Calculate calc2 = new Calculate(15, 2);
+            Console.WriteLine($"{calc2.Tostring()}");
+            Console.WriteLine("Введите первое число: ");
+            double num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число: ");
+            double num2 = double.Parse(Console.ReadLine());
+            Calculate calc3 = new Calculate(num1, num2);
+            Console.WriteLine($"{calc3.Tostring()}");
+           
+
+            Console.ReadKey();
+        }
+    }
+}
